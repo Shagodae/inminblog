470213bc34a09de766a930c43283990eaa784176
Shagodae <antrikshanautiyal@gmail.com>
2025-08-12 21:50:43 +0530

    Project commited

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes} from 'react-router-dom'
+import { Route, Routes, Navigate} from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
 import Dashboard from './pages/admin/Dashboard'
